38b90452e551f9359e07a4dea7bf23c8c30e594c
Adam Sales <asales@utexas.edu>
2017-09-04 17:58:55 -0500

    accounted for missing usage
    only compare students within schools where missingness is (roughly) the
    same across years
    
    plot unit usage across years

--- fig1.r
+++ fig1.r
@@ -16,17 +16,31 @@ dataA <- data
 
 
 
-pload('~/Box Sync/CT/data/problemLevelUsageData/probLevelData.RData')
+load('~/Box Sync/CT/data/problemLevelUsageData/probLevelData.RData')
+load('~/Box Sync/CT/data/sectionLevelUsageData/advanceDataWfinalinc.RData')
+
+
+usageIDs <- unique(c(unique(x$field_id),unique(advance$field_id)))
+
 hs1 <- read.csv('~/Box Sync/CT/data/RANDstudyData/H1_algebra_rcal_20121119_fieldid.csv')
 hs2 <- read.csv('~/Box Sync/CT/data/RANDstudyData/H2_algebra_rcal_20121119_fieldid.csv')
-
-
 
 stopifnot(all.equal(names(hs1)[1:100],names(hs2)[1:100]))
 
 stud <- rbind(hs1[,1:100],hs2[!hs2$field_id%in%hs1$field_id,1:100])
 stud$field_id <- c(hs1$field_id,hs2$field_id[!hs2$field_id%in%hs1$field_id])
 stud <- stud[stud$treatment==1,]
+
+stud$obsUsage <- stud$field_id%in%usageIDs
+
+schoolMiss <- NULL
+for(scl in unique(stud$schoolid2)){
+ schoolMiss <- rbind(schoolMiss,with(stud,
+  c(mean(obsUsage[year==1 & schoolid2==scl]),
+    mean(obsUsage[year==2 & schoolid2==scl]))))}
+rownames(schoolMiss) <- unique(stud$schoolid2)
+schoolMiss <- cbind(schoolMiss,diff=schoolMiss[,1]-schoolMiss[,2])
+smallDiff <- rownames(schoolMiss)[!is.na(schoolMiss[,'diff']) & abs(schoolMiss[,'diff'])<0.4 & schoolMiss[,1]>0& schoolMiss[,2]>0]
 
 twice <- intersect(hs1$field_id,hs2$field_id)
 
@@ -44,7 +58,6 @@ prob$curriculum[grep('cc',prob$curriculum)] <- 'CC'
 prob$curriculum <- sub('del_','',prob$curriculum)
 prob$curriculum <- factor(prob$curriculum)
 
-load('~/Box Sync/CT/data/sectionLevelUsageData/advanceData.RData')
 adv <- advance[advance$field_id%in%stud$field_id,]
 for(vv in names(adv)) if(is.factor(adv[[vv]])) levels(adv[[vv]]) <- tolower(levels(adv[[vv]]))
 adv$curriculum <- as.character(adv$curriculum)
@@ -73,13 +86,24 @@ data <- full_join(data,stud)
 data <- data[!grepl('test',data$section,ignore.case=TRUE),]
 
 data$overall <- ifelse(data$curriculum=='CC',
- 'Customized','Standard')
+                       'Customized','Standard')
+
+
+##########################################################
+### should we remove schools where usage missingness was very
+### different between years 1 & 2??
+data <- data[data$schoolid2%in%smallDiff,]
+#############################################
+
+levels(data$state) <- names(sort(table(stud$state[stud$schoolid2%in%smallDiff])))
+
+
 
 secByStud <- summarize(group_by(data,state,year,field_id,year),numSec=n_distinct(section,na.rm=TRUE))
 secByStud$numSec[secByStud$numSec==0] <- NA
 
 secByStud$yearFac <- c('Yr 1','Yr 2')[secByStud$year]
-secByStud$Curriculum <- secByStud$overall
+
 
 secStateYear <- ggplot(secByStud,aes(yearFac,numSec))+geom_boxplot()+facet_grid(~state)+ylim(0,250)+labs(x='',y='Number of Sections Per Student')
 ggsave('fig1.jpg',width=6,height=3)
@@ -88,31 +112,69 @@ ggsave('fig1.jpg',width=6,height=3)
 #ggsave('fig1a.jpg',width=6,height=3)
 
 
-cc <- data %>% group_by(state,year,field_id,section
+cc <- data %>% group_by(state,year,field_id,section) %>% summarize(curr=mean(overall=='Customized',na.rm=TRUE))
+perCCdat <- cc %>% filter(state%in%c('KY','TX','MI')) %>% group_by(state,year)%>% summarize(perCC=mean(curr,na.rm=TRUE))
 
-perCCdat <- data%>% filter(state%in%c('KY','TX','MI')) %>% group_by(state,year,section,overall) %>% mutate(perCC= mean(overall=='Customized'))
+#perCCdat <- data%>% filter(state%in%c('KY','TX','MI')) %>% group_by(state,year,section,overall) %>% mutate(perCC= mean(overall=='Customized'))
 
-percCC <- ggplot(perCCdat,aes(yearFac,perCC))+geom_col()+facet_grid(~state)+labs(x='',y='% from Customized Curriculua')
+perCCdat$yearFac <- ifelse(perCCdat$year==1,'Yr 1','Yr 2')
+percCC <- ggplot(perCCdat,aes(yearFac,perCC))+geom_col()+facet_grid(~state)+labs(x='',y='% Sections from Customized Curriculua')
 ggsave('percentCC.jpg',width=6,height=3)
 
-sectionLevelDat <- data%>% select(field_id,year,curriculum,unit,section,version,state,schoolidn,grdlvl,grade,teachid,classid,overall,date,total_t1,status) %>%
- group_by(field_id,year,curriculum,unit,section,version) %>%
- summarize(state=state[1],schoolidn=schoolidn[1],grdlvl=grdlvl[1],grade=grade[1],teachid=teachid[1],classid=classid[1],overall=overall[1],dateBegin=min(date),dateEnd=max(date),time=sum(total_t1),status=status[1])
+sectionLevelDat <- data%>% group_by(field_id,unit,section) %>% summarize(state=state[1],schoolidn=schoolidn[1],grdlvl=grdlvl[1],grade=grade[1],teachid=teachid[1],classid=classid[1],customized=mean(overall=='Customized',na.rm=TRUE),dateBegin=min(date,na.rm=TRUE),dateEnd=max(date,na.rm=TRUE),time=sum(total_t1,na.rm=TRUE),status=status[1],ndays=n_distinct(date,na.rm=TRUE),year=year[1],nprob=n_distinct(Prob1,na.rm=TRUE))
 
-for(nn in c("curriculum",  "unit" ,       "section", "version"))
- sectionLevelDat[[nn]] <- factor(tolower(as.character(sectionLevelDat[[nn]])))
-
-sectionLevelDat <- merge(sectionLevelDat,advance,all.x=TRUE,all.y=FALSE)
+sectionLevelDat <- within(sectionLevelDat,{
+                          nprob[nprob==0] <- NA
+                          time[time==0] <- NA
+                          ndays[ndays==0] <- NA
+                      })
 
 ### mastered sections by state/year
-mastSec <- sectionLevelDat%>%group_by(state,year,field_id,year,overall)%>%summarize(nGrad=mean(status=='graduated',na.rm=TRUE)*n())
+mastSec <- sectionLevelDat%>%group_by(state,year,field_id,year)%>%summarize(nGrad=mean(status=='graduated',na.rm=TRUE)*n())
 mastSec$yearFac <- c('Yr 1','Yr 2')[mastSec$year]
 
-mastStateYear <- ggplot(mastSec,aes(yearFac,nGrad))+geom_boxplot()+facet_grid(~state)+labs(x='',y='# Sections Mastered Per Student')+ylim(0,250)
-ggsave('fig1.jpg',width=6,height=3)
-
-
-secStateYear <- ggplot(subset(secByStud,status=='graduated'),aes(yearFac,numSec))+geom_boxplot()+facet_grid(~state)+ylim(0,200)+labs(x='',y='Number of Sections Per Student')
+mastStateYear <- ggplot(mastSec,aes(yearFac,nGrad))+geom_boxplot()+facet_grid(~state)+labs(x='',y='# Sections Mastered Per Student')+ylim(0,200)
 ggsave('fig3.jpg',width=6,height=3)
 
+### which units were worked, by year
+sectionLevelDat$unit[grep('unit-conversions',sectionLevelDat$unit)] <- 'unit-conversions'
+unitLevel <- sectionLevelDat%>%group_by(field_id,unit)%>%summarize(state=state[1],schoolidn=schoolidn[1],grdlvl=grdlvl[1],grade=grade[1],teachid=teachid[1],classid=classid[1],customized=mean(customized,na.rm=TRUE),dateBegin=min(dateBegin,na.rm=TRUE),dateEnd=max(dateEnd,na.rm=TRUE),time=sum(time,na.rm=TRUE),cp=mean(status=='changed placement',na.rm=TRUE),grad=mean(status=='graduated',na.rm=TRUE),prom=mean(status=='promoted',na.rm=TRUE),finc=mean(status=='final_or_incomplete',na.rm=TRUE),ndays=sum(ndays,na.rm=TRUE),year=year[1],nprob=sum(nprob,na.rm=TRUE))
 
+curricula <- read.csv('~/Box Sync/CT/data/sectionLevelUsageData/RAND_study_curricula.csv',stringsAsFactors=FALSE)
+curricula <- subset(curricula,curriculum_name=='algebra i')
+curricula$unit <- tolower(curricula$unit)
+curricula <- subset(curricula,unit%in%intersect(curricula$unit[curricula$ct=='2007'],curricula$unit[curricula$ct=='2008r1']))
+units <- curricula$unit[curricula$ct=='2007']
+
+
+
+workedUnits <- table(unitLevel$unit,unitLevel$year)[units,]
+workedUnits <- data.frame(numWorked=c(workedUnits[,1],workedUnits[,2]),year=c(rep('Year 1',length(units)),rep('Year 2',length(units))),
+                          Unit=factor(rep(units,2),units))
+workedUnits$meanWorked <- workedUnits$numWorked/ifelse(workedUnits$year=='Year 1',
+                                                       n_distinct(unitLevel$field_id[unitLevel$year==1 & !is.na(unitLevel$unit)]),
+                                                       n_distinct(unitLevel$field_id[unitLevel$year==2 & !is.na(unitLevel$unit)]))
+
+
+unitsWorked <- ggplot(workedUnits,aes(x=Unit,y=meanWorked,color=year,group=year))+geom_point()+geom_line()+theme(axis.text.x = element_text(angle = 90, hjust = 1,vjust=0.5))
+
+
+### just to try it in a different way...
+## use1 <- rbind(x[x$field_id%in%unique(data$field_id[data$year==1]),c('field_id','unit')],
+##               advance[advance$field_id%in%unique(data$field_id[data$year==1]),c('field_id','unit')])
+## use1 <- use1[!duplicated(use1),]
+## use1$year <- 1
+
+## use1S <- split(use1,use1$field_id)
+## means1 <- sapply(units,function(unit) mean(sapply(use1S,function(sss) is.element(unit,sss$unit))))
+
+## use2 <- rbind(x[x$field_id%in%unique(data$field_id[data$year==2]),c('field_id','unit')],
+##               advance[advance$field_id%in%unique(data$field_id[data$year==2]),c('field_id','unit')])
+## use2 <- use2[!duplicated(use2),]
+## use2$year <- 2
+
+## use2S <- split(use2,use2$field_id)
+## means2 <- sapply(units,function(unit) mean(sapply(use2S,function(sss) is.element(unit,sss$unit))))
+
+## workedUnits$mean2 <- c(means1,means2)
+#### OK it works (except for "unit conversions" which I messed around with)
---
+++ missingUsage.r
@@ -0,0 +1,108 @@
+library(dplyr)
+library(lme4)
+
+### we have more usage data in year 1 than year 2
+print(load('~/Box Sync/CT/data/sectionLevelUsageData/advanceData.RData'))
+
+advance%>%group_by(year)%>%summarize(n_distinct(field_id))
+
+print(load('~/Box Sync/CT/data/problemLevelUsageData/probLevelData.RData'))
+
+
+x%>%group_by(study.year)%>%summarize(n_distinct(field_id))
+
+
+#### what about as percentage of students in RAND dataset?
+
+hs1 <- read.csv('~/Box Sync/CT/data/RANDstudyData/H1_algebra_rcal_20121119_fieldid.csv')
+hs2 <- read.csv('~/Box Sync/CT/data/RANDstudyData/H2_algebra_rcal_20121119_fieldid.csv')
+
+### treated students in RAND dataset:
+ids <- unique(c(hs1$field_id[hs1$treatment==1],hs2$field_id[hs2$treatment==1]))
+year <- ifelse(ids%in%hs1$field_id,1,2)
+
+### "advance" dataset:
+mean(ids[year==1]%in%advance$field_id)
+mean(ids[year==2]%in%advance$field_id)
+
+### "problem level" dataset:
+mean(ids[year==1]%in%x$field_id)
+mean(ids[year==2]%in%x$field_id)
+
+### both:
+usageIDs <- unique(c(unique(x$field_id),unique(advance$field_id)))
+mean(ids[year==1]%in%usageIDs)
+mean(ids[year==2]%in%usageIDs)
+
+### (is this just because I counted "both-year" students as yr-1?)
+length(intersect(hs1$field_id,hs2$field_id))
+(sum(ids[year==2]%in%usageIDs)+193)/(length(ids[year==2])+193)
+
+### what predicts missingness?
+stopifnot(all.equal(names(hs1)[1:100],names(hs2)[1:100]))
+
+stud <- rbind(hs1[,1:100],hs2[!hs2$field_id%in%hs1$field_id,1:100])
+stud$field_id <- c(hs1$field_id,hs2$field_id[!hs2$field_id%in%hs1$field_id])
+stud <- stud[stud$treatment==1,]
+
+stud$obsUsage <- stud$field_id%in%usageIDs
+
+Mode <- function(x) levels(x)[which.max(table(x))]
+
+## median/mode imputation for predictors, with NA flags:
+studImp1 <- within(subset(stud,year==1),{
+                   gradeNA <- is.na(grade)
+                   grade[is.na(grade)] <- median(grade,na.rm=TRUE)
+                   raceNA <- is.na(race)
+                   race[is.na(race)] <- Mode(race)
+                   sexNA <- is.na(sex)
+                   sex[is.na(sex)] <- 'F'
+                   specedNA <- is.na(spec_speced)
+                   spec_speced[is.na(spec_speced)] <- 0
+                   giftedNA <- is.na(spec_gifted)
+                   spec_gifted[is.na(spec_gifted)] <- 0
+                   eslNA <- is.na(spec_esl)
+                   spec_esl[is.na(spec_esl)] <- 0
+                   frlNA <- is.na(frl)
+                   frl[is.na(frl)] <- 1
+                   xirtNA <- is.na(xirt)
+                   xirt[is.na(xirt)] <- median(xirt,na.rm=TRUE)})
+studImp2 <- within(subset(stud,year==2),{
+                   gradeNA <- is.na(grade)
+                   grade[is.na(grade)] <- median(grade,na.rm=TRUE)
+                   raceNA <- is.na(race)
+                   race[is.na(race)] <- Mode(race)
+                   sexNA <- is.na(sex)
+                   sex[is.na(sex)] <- 'F'
+                   specedNA <- is.na(spec_speced)
+                   spec_speced[is.na(spec_speced)] <- 0
+                   giftedNA <- is.na(spec_gifted)
+                   spec_gifted[is.na(spec_gifted)] <- 0
+                   eslNA <- is.na(spec_esl)
+                   spec_esl[is.na(spec_esl)] <- 0
+                   frlNA <- is.na(frl)
+                   frl[is.na(frl)] <- 1
+                   xirtNA <- is.na(xirt)
+                   xirt[is.na(xirt)] <- median(xirt,na.rm=TRUE)})
+
+studImp <- rbind(studImp1,studImp2)
+
+## missingness due to year?
+summary(glmer(obsUsage~year+(1+year|schoolid2),family=binomial,data=stud))
+
+## due to changes in composition?
+summary(mainEffMod <- glmer(obsUsage~state+grade+gradeNA+race+raceNA+sex+sexNA+spec_speced+specedNA+spec_gifted+spec_esl+eslNA+frl+frlNA+xirt+xirtNA+year+(1|schoolid2),data=studImp,family=binomial))
+
+summary(missMod <- glmer(obsUsage~(state+grade+gradeNA+race+raceNA+sex+sexNA+spec_speced+specedNA+spec_gifted+spec_esl+eslNA+frl+frlNA+xirt+xirtNA)*year+(1|schoolid2),data=studImp,family=binomial))
+
+schoolMiss <- NULL
+for(scl in unique(studImp$schoolid2)){
+ schoolMiss <- rbind(schoolMiss,with(stud,
+  c(mean(obsUsage[year==1 & schoolid2==scl]),
+    mean(obsUsage[year==2 & schoolid2==scl]))))}
+rownames(schoolMiss) <- unique(studImp$schoolid2)
+schoolMiss <- cbind(schoolMiss,diff=schoolMiss[,1]-schoolMiss[,2])
+smallDiff <- rownames(schoolMiss)[!is.na(schoolMiss[,'diff']) & abs(schoolMiss[,'diff'])<0.4 & schoolMiss[,1]>0& schoolMiss[,2]>0]
+
+
+
